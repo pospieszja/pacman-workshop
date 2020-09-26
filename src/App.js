bbbf54822107bf5790dea5a54fc88e6f4f0bd518
pospieszja <j.pospieszynski@outlook.com>
2020-09-26 10:44:56 +0200

    Add Board component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {useState} from 'react';
 import React from 'react';
 
 import Header from './components/Header';
+import Board from './components/Board';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header score={score}/>
+      <Board/>
     </div>
   );
 }
